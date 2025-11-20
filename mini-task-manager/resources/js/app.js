26b730832d4340b7de8f4e11498ea2736b338c29
JESUS MAGDIAL MENDEZ GUZMAN <119821074+Jesusguz@users.noreply.github.com>
2025-11-20 12:23:55 -0600

    Update app.js

--- mini-task-manager/resources/js/app.js
+++ mini-task-manager/resources/js/app.js
@@ -2,10 +2,10 @@
 import './bootstrap';
 import './bootstrap';
 import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
-import Sortable from 'sortablejs';
+//import Sortable from 'sortablejs';
 
 window.Alpine = Alpine;
-window.Sortablejs = Sortable;
+//window.Sortablejs = Sortable;
 
 Livewire.start();
 Alpine.start();
